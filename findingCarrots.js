0e65aa5dccdef4ac6d54e937ff39b507aecd0ef4
Jin Hang <jinhang@query.ai>
2019-12-01 00:21:40 -0600

    Scrape date

--- findingCarrots.js
+++ findingCarrots.js
@@ -1,8 +1,10 @@
-var request = require("request");
-var cheerio = require("cheerio");
-var URL = require("url-parse");
+const request = require("request");
+const cheerio = require("cheerio");
+const URL = require("url-parse");
+const admin = require("firebase-admin");
+const moment = require("moment");
 
-var pageToVisit = "https://www.sdstate.edu/events/list";
+const pageToVisit = "https://www.sdstate.edu/events/list";
 console.log("Visiting page: ", pageToVisit);
 request(pageToVisit, function(error, response, body) {
    if (error) {
@@ -51,9 +53,19 @@ function collectEvents($) {
     $(timeToken).each((idx, elem) => {
         let str = $(elem).find('span.metadata--event').slice(0, 1).text();
         str = str.trim();
-        console.log("TIME STRING: ");
-        console.log(str);
-        objAry[idx]['start_time'] = str;
+        const commaIdx = str.indexOf('‚');
+        const dashIdx = str.indexOf('–');
+        const dayMonth = str.slice(0, commaIdx);
+        const startTime = str.slice(commaIdx + 1, dashIdx);
+        const endTime = str.slice(dashIdx + 1);
+        const objWithDate = moment(dayMonth, ['MMM. D', 'MMM. DD']).toDate();
+        const objWithStartTime = moment(startTime, ['h:m a']).toDate();
+        const objWithEndTime = moment(endTime, ['h:m a']).toDate();
+        const newStartTime = objWithDate.setTime(objWithStartTime.getTime());
+        const newEndTime = objWithDate.setTime(objWithEndTime.getTime());
+
+        objAry[idx]['start_time'] = admin.firestore.Timestamp.fromDate(new Date(newStartTime));
+        objAry[idx]['end_time'] = admin.firestore.Timestamp.fromDate(new Date(newEndTime));
     });
 
     console.log(">>> RESULT: ");
