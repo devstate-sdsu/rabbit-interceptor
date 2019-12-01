05675d90bd495825dbd39ba27117c3c31dc3c227
Jin Hang <jinhang@query.ai>
2019-12-01 01:43:58 -0600

    Scrape from all pages

--- findingCarrots.js
+++ findingCarrots.js
@@ -1,25 +1,40 @@
-const request = require("request");
+const request = require("request-promise");
 const cheerio = require("cheerio");
 const URL = require("url-parse");
 const admin = require("firebase-admin");
 const moment = require("moment");
 
-const pageToVisit = "https://www.sdstate.edu/events/list";
-console.log("Visiting page: ", pageToVisit);
-request(pageToVisit, function(error, response, body) {
-   if (error) {
-       console.log("Error: " + error);
-   }
-    console.log("Status code: " + response.statusCode);
-   if (response.statusCode == 200) {
-       // Parse the document body
-       var $ = cheerio.load(body);
-       console.log("Page title: " + $('title').text());
-       collectEvents($);
-   }
-});
 
-function collectEvents($) {
+main().then((res) => console.log("THIS IS RESULT: ", res));
+
+async function main() {
+    const base = "https://www.sdstate.edu/events/list?department=All&title=&page=";
+    let masterAry = [];
+    for (let i = 0; i < 11 ; i++) {
+        const pageToVisit = base + i.toString();
+        console.log("Visiting page: ", pageToVisit);
+        await request(pageToVisit, function(error, response, body) {
+            if (error) {
+                console.log("Error: " + error);
+            }
+            console.log("Status code: " + response.statusCode);
+            if (response.statusCode == 200) {
+                // Parse the document body
+                let $ = cheerio.load(body);
+                console.log("Page title: " + $('title').text());
+                const aryFromPage = collectEvents($);
+                if (aryFromPage.length === 0) {
+                    return;
+                } else {
+                    masterAry = masterAry.concat(aryFromPage);
+                }
+            }
+        });
+    }
+    return masterAry;
+}
+
+function collectEvents($, baseYear) {
     const objAry = [];
 
     // Scrape title
@@ -59,14 +74,19 @@ function collectEvents($) {
         const startTime = str.slice(commaIdx + 1, dashIdx);
         const endTime = str.slice(dashIdx + 1);
         const objWithDate = moment(dayMonth, ['MMM. D', 'MMM. DD']).toDate();
-        const objWithStartTime = moment(startTime, ['h:m a']).toDate();
-        const objWithEndTime = moment(endTime, ['h:m a']).toDate();
+        const objWithStartTime = moment(startTime, ['hh:mm a', 'h:mm a']).toDate();
+        const objWithEndTime = moment(endTime, ['hh:mm a', 'h:mm a']).toDate();
         const newStartTime = objWithDate.setTime(objWithStartTime.getTime());
         const newEndTime = objWithDate.setTime(objWithEndTime.getTime());
 
         objAry[idx]['time'] = []
-        objAry[idx]['time'].push(admin.firestore.Timestamp.fromDate(new Date(newStartTime)));
-        objAry[idx]['time'].push(admin.firestore.Timestamp.fromDate(new Date(newEndTime)));
+        try {
+            objAry[idx]['time'].push(admin.firestore.Timestamp.fromDate(new Date(newStartTime)));
+            objAry[idx]['time'].push(admin.firestore.Timestamp.fromDate(new Date(newEndTime)));
+        } catch {
+            objAry[idx]['time'].push(admin.firestore.Timestamp.fromDate(new Date()));
+            objAry[idx]['time'].push(admin.firestore.Timestamp.fromDate(new Date()));
+        }
         // Set time updated
         objAry[idx]['time_updated'] = admin.firestore.Timestamp.fromDate(new Date());
         // Set update note
@@ -87,7 +107,5 @@ function collectEvents($) {
             objAry[idx]['image'] = 'https://www.sdstate.edu/sites/default/files/images/Mon-19/Artboard%205%402x_1.png';
         }
     });
-
-    console.log(">>> RESULT: ");
-    console.log(objAry);
+    return objAry;
 }
