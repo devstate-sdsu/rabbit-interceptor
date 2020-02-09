0085220ce9d11f164ed5a2cf39196fca0c4e6e5e
Jin Hang <jinhang@query.ai>
2020-02-09 17:17:19 -0600

    add project id

--- findingCarrots.js
+++ findingCarrots.js
@@ -9,6 +9,7 @@ admin.initializeApp({
     credential: admin.credential.cert({
       "private_key": process.env.FIREBASE_PRIVATE_KEY,
       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+      "project_id": process.env.FIREBASE_PROJECT_ID
     }),
     databaseURL: "https://rabbitbums.firebaseio.com/"
   });
